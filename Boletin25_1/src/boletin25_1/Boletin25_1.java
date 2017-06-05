521175a193e63503fde3d274c9cf1d96dc762ef8
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-06-05 19:21:46 +0200

    Hace la ventana visible

---
+++ Boletin25_1/src/boletin25_1/Boletin25_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25_1;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin25_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana objVen=new Ventana();
+        objVen.setVisible(true);
+    }
+    
+}
